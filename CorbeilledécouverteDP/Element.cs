84b1a633f8aadbd0625100e6bedf43d6cf359483
TomFREBAULT <tom.frebault@viacesi.fr>
2025-02-02 15:23:41 +0100

    Premiere etape

--- CorbeilledécouverteDP/Element.cs
+++ CorbeilledécouverteDP/Element.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CorbeilledécouverteDP
+using System;
+
+namespace Element
 {
-    internal class Element
+    // Classe de base pour représenter un élément chimique
+    public abstract class ElementBase
     {
+        // Propriétés communes
+        public string Nom { get; set; }
+        public string Symbole { get; set; }
+        public double Masse { get; set; }
+        
+        // La position de l'élément dans l'univers (coordonnées X, Y)
+        public (int X, int Y) Position { get; set; }
+
+        protected ElementBase(string nom, string symbole, double masse)
+        {
+            Nom = nom;
+            Symbole = symbole;
+            Masse = masse;
+        }
+
+        // Affiche les informations de l'élément
+        public override string ToString()
+        {
+            return $"{Nom} ({Symbole}) - Masse : {Masse} - Position : ({Position.X}, {Position.Y})";
+        }
+    }
+
+    // Classe concrète pour représenter un atome
+    public class Atome : ElementBase
+    {
+        public Atome(string nom, string symbole, double masse) 
+            : base(nom, symbole, masse)
+        {
+        }
     }
 }
